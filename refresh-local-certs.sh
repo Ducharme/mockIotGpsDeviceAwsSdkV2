5f685530ead43417e0bd7a323ad9fcd882518627
claude-at-748293476463 <claude.ducharme@gmail.com>
2022-10-11 20:41:05 +0800

    Add certs to sub folder mockIotGpsDeviceAwsSdkV2

--- refresh-local-certs.sh
+++ refresh-local-certs.sh
@@ -5,8 +5,8 @@ AWS_ACCOUNT_ID_VALUE=$(aws sts get-caller-identity --query "Account" --output te
 S3_OBJECT_STORE=$PROJECT_NAME-object-store-$AWS_ACCOUNT_ID_VALUE
 
 mkdir ./certs
-aws s3 cp s3://$S3_OBJECT_STORE/certs/certificate-id.txt ./certs
-aws s3 cp s3://$S3_OBJECT_STORE/certs/certificate.pem.crt ./certs
-aws s3 cp s3://$S3_OBJECT_STORE/certs/private.pem.key ./certs
-aws s3 cp s3://$S3_OBJECT_STORE/certs/public.pem.key ./certs
-aws s3 cp s3://$S3_OBJECT_STORE/certs/root-ca.crt ./certs
+aws s3 cp s3://$S3_OBJECT_STORE/certs/mockIotGpsDeviceAwsSdkV2/certificate-id.txt ./certs
+aws s3 cp s3://$S3_OBJECT_STORE/certs/mockIotGpsDeviceAwsSdkV2/certificate.pem.crt ./certs
+aws s3 cp s3://$S3_OBJECT_STORE/certs/mockIotGpsDeviceAwsSdkV2/private.pem.key ./certs
+aws s3 cp s3://$S3_OBJECT_STORE/certs/mockIotGpsDeviceAwsSdkV2/public.pem.key ./certs
+aws s3 cp s3://$S3_OBJECT_STORE/certs/mockIotGpsDeviceAwsSdkV2/root-ca.crt ./certs
